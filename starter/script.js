b9f5cdf8602284916031c43fe1799a97bd8f585e
luul negasi <luultesfay24@gmail.com>
2021-09-06 09:30:04 -0700

    challage #4 solved

--- starter/script.js
+++ starter/script.js
@@ -332,7 +332,7 @@ Ford.speedUS = 50;
 console.log(Ford); //we set the speed to new speed  Object { make: "Ford", speed: 80 }
 
 ///INHERITANCE BETWEEN CLASSES
-//CONASTRUCTOR FUNCTION
+//using CONASTRUCTOR FUNCTION
 //WE will link  both classes prototype
 
 const Human = function (firstName, birthYear) {
@@ -371,3 +371,65 @@ const tiemar = new Student('tiemar', 1998, 'science');
 console.log(tiemar);
 tiemar.introduce();
 tiemar.calcAge(); //39
+
+///Challenge #4
+/*
+Your tasks:
+1. Use a constructor function to implement an Electric Car (called 'EV') as a child
+"class" of 'Car'. Besides a make and current speed, the 'EV' also has the
+current battery charge in % ('charge' property)
+2. Implement a 'chargeBattery' method which takes an argument
+'chargeTo' and sets the battery charge to 'chargeTo'
+3. Implement an 'accelerate' method that will increase the car's speed by 20,
+and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140
+km/h, with a charge of 22%'
+4. Create an electric car object and experiment with calling 'accelerate',
+'brake' and 'chargeBattery' (charge to 90%). Notice what happens when
+you 'accelerate'! Hint: Review the definiton of polymorphism 😉
+Test data:
+§ Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+*/
+
+const CarTest = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+CarTest.prototype.accelerates = function () {
+  const newSpeed = this.speed + 20;
+  console.log(`${this.make} accelerates at the speed of ${newSpeed}  km`); //
+};
+
+CarTest.prototype.breaks = function () {
+  const newSpeed = this.speed - 5;
+  console.log(`${this.make} brakes at the speed of ${newSpeed}  km`); //
+};
+
+const EV = function (make, speed, charge) {
+  CarTest.call(this, make, speed);
+  this.charge = charge;
+};
+
+//linking prototype
+EV.prototype = Object.create(CarTest.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+//we will override the accelerate that is in prototype of parent class   by the new accelerate
+//we are observing her polymorphisem
+EV.prototype.accelerates = function () {
+  //we have 2 the same method in the prototype chain the one who is first in the chain override the other one in this case child over ride the prototype of parent class
+  this.speed += 30;
+  this.charge--;
+  console.log(
+    `${this.make} accelerates at the speed of ${this.speed}  km/h and  battery ${this.charge} %`
+  );
+};
+const tesla = new EV('tesla', 120, 23);
+console.log(tesla);
+
+tesla.chargeBattery(70); //70  sets to 70
+tesla.accelerates();
+tesla.breaks();
+console.log(tesla);
