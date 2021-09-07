eaa28d17e5576396e0a659e000273a74dc038200
luul negasi <luultesfay24@gmail.com>
2021-09-07 08:44:04 -0700

    Encapsulation: Private Class Fields and Methods

--- starter/script.js
+++ starter/script.js
@@ -535,25 +535,34 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movement = [];
+
+    //protected property
+    this._pin = pin;
+    this._movement = [];
+
+    //local property
     this.locale = navigator.language;
   }
   //public interface
+
+  getMovment() {
+    return this._movement;
+  }
   deposit(mov) {
-    this.movement.push(mov);
+    this._movement.push(mov);
   }
 
   withDrow(mov) {
     this.deposit(-mov);
   }
 
-  loanApprove(mov) {
+  _loanApprove(mov) {
+    //we prtect this from breaching outside  so this is protected  not public api
     return true;
   }
 
   requestLoan(mov) {
-    if (this.loanApprove) this.deposit(mov);
+    if (this._loanApprove) this.deposit(mov);
   }
 }
 const Acc1 = new Account('luul', 'USD', 1111);
@@ -563,3 +572,96 @@ Acc1.deposit(100); //100 deposit
 Acc1.withDrow(500); //-500
 console.log(Acc1); //Object { owner: "luul", currency: "USD", pin: 1111, movement: (2) […], locale: "en-US" }
 Acc1.requestLoan(1000); //  1000 then request approved
+
+console.log(Acc1.getMovment()); //Array(3) [ 100, -500, 1000 ]  using getMovment method we protect the data from manuplated or abused outside , so no one can change the internal data
+
+//Encapsulation: Protected Properties and Methods    using ' _ '  in the obove code
+
+/*
+//////////////////Encapsulation: Private Class Fields and Methods   video 221
+
+
+/ Encapsulation: Protected Properties and Methods
+// Encapsulation: Private Class Fields and Methods
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static version)
+
+class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+
+    // Protected property
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  requestLoan(val) {
+    // if (this.#approveLoan(val)) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+      return this;
+    }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+// acc1.approveLoan(1000);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+Account.helper();
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+
+*/
